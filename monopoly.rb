76741e117b84c572c553e9480bc144c764bf204e
MatthewVenzon <matthew.venzon@gmail.com>
2014-01-29 14:58:34 -0600

    Answers to HW # 3 Venzon

--- monopoly.rb
+++ monopoly.rb
@@ -15,23 +15,28 @@ special_squares = { :good => ["Go", "Free Parking", "Just Visiting"],
 #A1: Here's the answer:
 puts cheap_property[:price]
 
+
 #Q2. How much does it cost to buy Boardwalk?
 #A2: Your code goes here:
-
+puts rich_property[:name]
 
 
 #Q3. How much is rent for Baltic?
 #A3: Your code goes here:
-
+puts cheap_property[:rent]
 
 
 #Q4. How much does it cost to buy New York Ave?
 #A4: Your code goes here:
-
+puts more_properties.at(2)[:price]
 
 
 #Q5: How many "good" special squares are there?
 #A5: Your code goes here:
+puts special_squares[:good].count
+
+
+
 
 
 
